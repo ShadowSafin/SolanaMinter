288ddaaeb2f39046e4a8c759794af43b7fbad895
ShadowSafin <104557943+ShadowSafin@users.noreply.github.com>
2025-04-07 02:05:45 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-
 import "./polyfills"; // Import polyfills first
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { SpeedInsights } from "@vercel/speed-insights/react"; // <-- Add this import
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <SpeedInsights /> {/* <-- Add this line */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
